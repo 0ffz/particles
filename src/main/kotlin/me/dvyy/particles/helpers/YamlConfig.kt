671d4289207eacf726107b7d1402de10e8233896
Danielle Voznyy <dan.voznyy@gmail.com>
2025-01-22 17:39:21 -0500

    fix: YamlConfig always erroring on read

--- src/main/kotlin/me/dvyy/particles/helpers/YamlConfig.kt
+++ src/main/kotlin/me/dvyy/particles/helpers/YamlConfig.kt
@@ -7,8 +7,8 @@ class YamlConfig(input: InputStream) {
     val yaml = Yaml.default
     val node: YamlMap = yaml.parseToYamlNode(input).yamlMap
 
-    private fun get(key: String): YamlNode = key.split(".").fold(node) { acc, stringPart ->
-        acc.yamlMap[stringPart] ?: error("Key $key not found in config")
+    private fun get(key: String): YamlNode = key.split(".").fold(node) { acc: YamlNode, stringPart ->
+        acc.yamlMap.get<YamlNode>(stringPart) ?: error("Key $key not found in config")
     }
 
     fun propertyOrNull(key: String): String? = runCatching { property(key) }.getOrNull()
